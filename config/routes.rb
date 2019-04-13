ec73349ee4dad25bc8cd504835379c9fb7622f8e
ryo5678 <ryo5678craft@gmail.com>
2019-04-13 22:18:24 +0000

    View works

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'day_manager/index'
   get 'welcome/index'
   root 'welcome#index'
+  resources :day_manager
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
